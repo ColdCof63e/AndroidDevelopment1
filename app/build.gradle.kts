a1edeaf9ba088f7e414cbb4f4fbb8b8a33da7f95
Narendra B T <narendrabt93@gmail.com>
2025-07-13 20:36:04 -0400

    Added web services dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,15 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Dependencies for Web Services Lab 7
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    // Gson
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+
 }
