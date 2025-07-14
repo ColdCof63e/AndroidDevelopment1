60d4d7383646802710db517b29a4c1d19c5d734f
Narendra B T <narendrabt93@gmail.com>
2025-07-13 20:48:22 -0400

    Created and Weather Response data class

---
+++ app/src/main/java/com/bt/lab7webservices/models/WeatherResponse.kt
@@ -0,0 +1,23 @@
+package com.bt.lab7webservices.models
+
+data class WeatherResponse(
+    val weather: List<Weather>,
+    val base: String,
+    val main: Main,
+)
+
+data class Weather(
+    val id: Int,
+    val main: String,
+    val description: String,
+    val icon: String,
+)
+
+data class Main(
+    val temp: Double,
+    val feels_like: Double,
+    val temp_min: Double,
+    val temp_max: Double,
+    val pressure: Int,
+    val humidity: Int,
+)
