db4aae91754c9f64bcf8fb64c47d5d55b6a2e829
Narendra B T <narendrabt93@gmail.com>
2025-07-13 19:23:01 -0500

    adding boiler plate code for future references

---
+++ app/src/main/java/com/bt/boilerplate/views/StartScreen.kt
@@ -0,0 +1,64 @@
+package com.bt.boilerplate.views
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.bt.boilerplate.ui.theme.BoilerPlateTheme
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "💪 Stronger'n Better 💪",
+                        color = Color.White,
+                        style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Bold)
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Black,
+                    titleContentColor = Color.White
+                )
+            )
+        },
+        bottomBar = {
+            NavigationBar(containerColor = Color.Black, contentColor = Color.White) {
+            }
+        }
+    ) {
+            innerPadding ->
+        Greeting(
+            name = "Android",
+            modifier = Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BoilerPlateTheme {
+        Greeting("Android")
+    }
+}
