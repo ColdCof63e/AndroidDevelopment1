f3c500ab9120dda631453379fcbe8ed473876058
Narendra B T <narendrabt93@gmail.com>
2025-07-23 00:52:15 -0400

    added shared preferences in utils package

---
+++ app/src/main/java/com/bt/sharedPreferences/utils/SharedPreferences.kt
@@ -0,0 +1,24 @@
+package com.bt.sharedPreferences.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class UserSelection (context: Context) {
+    private val sharedPreferences = context.getSharedPreferences("user_selection", Context.MODE_PRIVATE)
+
+    var email: String
+        get() = sharedPreferences.getString("email", "") ?: ""
+        set(value) = sharedPreferences.edit().putString("email", value).apply()
+
+    var province: String
+        get() = sharedPreferences.getString("province", "") ?: ""
+        set(value) = sharedPreferences.edit().putString("province", value).apply()
+
+    var stocks: List<String>
+        get() = sharedPreferences.getStringSet("stocks", emptySet())?.toList() ?: emptyList()
+        set(value) = sharedPreferences.edit().putStringSet("stocks", value.toSet()).apply()
+
+    var subscription: String
+        get() = sharedPreferences.getString("subscription", "") ?: ""
+        set(value) = sharedPreferences.edit().putString("subscription", value).apply()
+}
