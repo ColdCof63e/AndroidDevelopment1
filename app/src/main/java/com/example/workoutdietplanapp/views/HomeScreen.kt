33aedbf0f1b0185b4f944942ab44c1ee20c3f4c9
Narendra B T <narendrabt93@gmail.com>
2025-08-06 02:13:38 -0400

    Finished Home Screen - hard coded few things, will be making changes in future

--- app/src/main/java/com/example/workoutdietplanapp/views/HomeScreen.kt
+++ app/src/main/java/com/example/workoutdietplanapp/views/HomeScreen.kt
@@ -3,15 +3,39 @@ package com.example.workoutdietplanapp.views
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FitnessCenter
 import androidx.compose.material.icons.filled.Logout
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -110,88 +134,105 @@ fun HomeScreen(navController: NavHostController, userViewModel: UserViewModel) {
             )
 
             // Foreground Content
-            Column(
+            LazyColumn(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(16.dp)
             ) {
-                Text(
-                    text = "Welcome, ${user.name}!",
-                    style = MaterialTheme.typography.headlineSmall,
-                    fontWeight = FontWeight.Bold,
-                    color = Color.White,
-                    modifier = Modifier.padding(bottom = 16.dp)
-                )
+                item {
+                    Text(
+                        text = "Welcome, ${user.name}!",
+                        style = MaterialTheme.typography.headlineSmall,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White,
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+                }
+
                 if(selectedExercise == null) {
-                    Column{
-                        exercises.forEach { exercise ->
-                            Card(
+                    items (exercises) { exercise ->
+                        val exerciseWorkouts = workouts.filter { it.workout == exercise }
+                        Card(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 8.dp),
+                            colors = CardDefaults.cardColors(
+                                containerColor = Color.White.copy(
+                                    alpha = 0.1f
+                                )
+                            ),
+                            shape = RoundedCornerShape(8.dp)
+                        ) {
+                            Box(
                                 modifier = Modifier
                                     .fillMaxWidth()
-                                    .padding(vertical = 8.dp),
-                                colors = CardDefaults.cardColors(
-                                    containerColor = Color.White.copy(
-                                        alpha = 0.1f
-                                    )
-                                ),
-                                shape = RoundedCornerShape(8.dp)
+                                    .clickable { selectedExercise = exercise }
+                                    .height(150.dp)
                             ) {
-                                Box(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .clickable { selectedExercise = exercise }
-                                        .padding(16.dp)
-                                ) {
-                                    Text(exercise, color = Color.White)
+                                Image(
+                                    painter = painterResource(id = getExerciseImage(exercise)),
+                                    contentDescription = null,
+                                    contentScale = ContentScale.FillWidth,
+                                    modifier = Modifier.fillMaxSize()
+                                )
+                                if(exerciseWorkouts.isNotEmpty()) {
+                                    Text(
+                                        "${exerciseWorkouts[0].day}\n${ exercise }",
+                                        color = Color.White,
+                                        modifier = Modifier
+                                            .padding(2.dp)
+                                            .background(
+                                            Color.Black.copy(alpha = 0.5f),
+                                            shape = RoundedCornerShape(8.dp)
+                                        )
+                                    )
                                 }
                             }
                         }
                     }
                 } else {
                     // Optionally, add a 'Back' button
-                    TextButton(onClick = { selectedExercise = null }) {
-                        Text("← Back to Exercises", color = Color.White)
+                    item {
+                        TextButton(onClick = { selectedExercise = null }) {
+                            Text("← Back to Exercises", color = Color.White)
+                        }
                     }
-
-                    // Filter workouts for the selected exercise
-                    val filteredWorkouts = workouts.filter { it.workout == selectedExercise }
-
-                    if (filteredWorkouts.isNotEmpty()) {
-                        LazyColumn {
-                            items(filteredWorkouts.size) { index ->
-                                val w = filteredWorkouts[index]
-                                Card(
+                    if (workouts.isNotEmpty()) {
+//                        items(workouts.size) { index ->
+//                            val w = workouts[index]
+//                        }
+                        item {
+                            Card(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(vertical = 8.dp),
+                                colors = CardDefaults.cardColors(containerColor = Color.White.copy(alpha = 0.1f)),
+                                shape = RoundedCornerShape(8.dp)
+                            ) {
+                                Row(
                                     modifier = Modifier
                                         .fillMaxWidth()
-                                        .padding(vertical = 8.dp),
-                                    colors = CardDefaults.cardColors(containerColor = Color.White.copy(alpha = 0.1f)),
-                                    shape = RoundedCornerShape(8.dp)
+                                        .padding(16.dp),
+                                    verticalAlignment = Alignment.CenterVertically
                                 ) {
-                                    Row(
-                                        modifier = Modifier
-                                            .fillMaxWidth()
-                                            .padding(16.dp),
-                                        verticalAlignment = Alignment.CenterVertically
-                                    ) {
-                                        Image(
-                                            painter = painterResource(id = getExerciseImage(selectedExercise!!)),
-                                            contentDescription = null,
-                                            modifier = Modifier.size(48.dp)
-                                        )
-                                        Text("Day ${w.day}", color = Color.White, fontWeight = FontWeight.Bold)
-                                        Text("  → ${w.variations} variations x ${w.reps} reps @ ${w.maxWeightKg}kg", color = Color.White)
-                                    }
+//                                    Text("  →${w.variations} variations x ${w.reps} reps @ ${w.maxWeightKg}kg", color = Color.White)
+                                    Text(
+                                        "Beginner:\n3 Variations x 3 reps @ 15kg" +
+                                                "\nIntermediate:\n3 Variations x 5 reps @ 20kg" +
+                                                "\nAdvance:\n5 Variations x 5 reps @ 35kg"
+                                    )
                                 }
                             }
                         }
                     } else {
-                        Text(
-                            text = "No workouts available for $selectedExercise.",
-                            color = Color.White
-                        )
+                        item {
+                            Text(
+                                text = "No workouts available for $selectedExercise.",
+                                color = Color.White
+                            )
+                        }
                     }
                 }
-
             }
         }
     }
